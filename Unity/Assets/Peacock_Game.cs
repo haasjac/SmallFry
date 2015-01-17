64790176507f4a5c41dc39e37f63edae27950d66
gatriex <haasjac@gmail.com>
2015-01-17 00:40:31 -0500

    Peacock_game Start

---
+++ Unity/Assets/Peacock_Game.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Peacock_Game : MonoBehaviour {
+
+	public float decrease_time = 0.5f;
+	private float game_time = 0;
+	private float switch_time = 0;
+	private float d_time = 0;
+	private int mash = 0;
+	private int button = 1;
+
+	// Use this for initialization
+	void Start () {
+		button = Mathf.FloorToInt(Random.Range (1, 4.99F));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (game_time < 30) {
+			if (game_time < switch_time) {
+			} else {
+				switch_time += Random.Range(1, 3);
+				//print ("switch" + game_time);
+				button = Mathf.FloorToInt(Random.Range (1, 4.99F));
+				print (button + " " + game_time);
+			}
+			//print (button);
+			if (Input.GetButtonDown ("Fire" + button)) {
+					mash ++;
+			}
+			game_time += Time.deltaTime;
+
+			if (d_time > decrease_time) {
+				d_time = 0;
+				mash --;
+			}
+			d_time += Time.deltaTime;
+		} else {
+			print ("done " + mash);
+		}
+
+	}
+}
