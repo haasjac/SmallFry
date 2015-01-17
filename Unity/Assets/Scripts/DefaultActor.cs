aa13def74f12052079c97c4b5e06b1cd741ca0ce
Cooper Riehl <cariehl@umich.edu>
2015-01-17 17:01:33 -0500

    Fixed exclamation point!

---
+++ Unity/Assets/Scripts/DefaultActor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SpaceJam
+{
+	public class DefaultActor : Actor
+	{
+		int state = 0;
+		
+		public override string GetName() { return "Benedict Cumberbatch"; }
+		
+		public override Sprite GetIcon() { return Resources.Load<Sprite>("Sprites/cumberbatch"); }
+		
+		public override string GetNextLine()
+		{
+			string line = null;
+			
+			switch (state)
+			{
+			case 0:
+				state = 1;
+				line = "I have nothing to say.";
+				break;
+			case 1:
+				state = 0;
+				break;
+			}
+			
+			return line;
+		}
+	}
+}
